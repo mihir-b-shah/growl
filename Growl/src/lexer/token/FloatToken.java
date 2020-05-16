b61273ded647db5ceb990ca3ada86a29839e5a77
mihir-b-shah <46288414+mihir-b-shah@users.noreply.github.com>
2020-05-16 13:03:10 -0500

    raaaaar

---
+++ Growl/src/lexer/token/FloatToken.java
@@ -0,0 +1,20 @@
+
+package lexer.token;
+
+/**
+ * For floating point numbers
+ * 
+ * @author mihir
+ */
+public class FloatToken extends Token {
+    public double token;
+    
+    public FloatToken(String tok) {
+        token = Double.parseDouble(tok);
+    }
+    
+    @Override
+    public Double getValue() {
+        return token;
+    }
+}
